93cedbba4bc0f34e559c412fedd38c55d2996d41
Mayank Bansal <mayank.bansal.0527@gmail.com>
2024-03-10 03:01:10 +0530

    nodemailer added

--- models/File.js
+++ models/File.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-
+const nodemailer = require("nodemailer")
+require("dotenv").config();
 const fileSchema = new mongoose.Schema({
     name: {
         type:String,
@@ -8,7 +9,7 @@ const fileSchema = new mongoose.Schema({
     email: {
         type:String,
     },
-    image: {
+    imageURL: {
         type:String,
     },
     tags: {
@@ -16,6 +17,34 @@ const fileSchema = new mongoose.Schema({
     }
 });
 
+//post middleware
+fileSchema.post("save", async function(doc) {
+    try {
+        console.log("DOC",doc);
+
+        //transporter
+        let transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass:process.env.MAIL_PASS,
+            },
+        })
+
+        //send mail
+        let info = await transporter.sendMail({
+            from:`Mayank`,
+            to:doc.email,
+            subject:"New file Uploaded on Cloudinary",
+            html:`<h2>Hello</h2> <p>File Uploaded</p>`,
+        })
+
+        console.log("INFO: ", info);
+    } catch (error) {
+        console.log(error);
+    }
+})
+  
 const File = mongoose.model("File", fileSchema);
 
 module.exports = File;
